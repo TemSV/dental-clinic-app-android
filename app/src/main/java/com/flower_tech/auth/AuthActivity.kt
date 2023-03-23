a0241a36865a4edd53b77a12c9d4a6866ce0ff03
makniker <102602253+makniker@users.noreply.github.com>
2023-03-23 16:43:26 +0300

    first draft

---
+++ app/src/main/java/com/flower_tech/auth/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.flower_tech.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.flower_tech.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
