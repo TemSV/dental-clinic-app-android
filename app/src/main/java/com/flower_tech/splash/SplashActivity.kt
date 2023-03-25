347d5ac3aad49c33087db79104776ab9a9a205e6
makniker <102602253+makniker@users.noreply.github.com>
2023-03-25 14:45:47 +0300

    SplashActivity.kt

--- app/src/main/java/com/flower_tech/splash/SplashActivity.kt
+++ app/src/main/java/com/flower_tech/splash/SplashActivity.kt
@@ -3,21 +3,15 @@ package com.flower_tech.splash
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import com.flower_tech.R
 import com.flower_tech.auth.AuthActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
-        Handler(Looper.getMainLooper()).postDelayed(
-            {
-                val intent = Intent(this, AuthActivity::class.java)
-                startActivity(intent)
-                finish()
-            },
-            3000)
+        val intent = Intent(this, AuthActivity::class.java)
+        startActivity(intent)
+        finish()
+
     }
+
 }
