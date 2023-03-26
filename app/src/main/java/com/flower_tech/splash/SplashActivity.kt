070bc141e716cb4a940478e3889cb4d1ebf081f7
makniker <102602253+makniker@users.noreply.github.com>
2023-03-26 23:03:25 +0300

    SplashActivity.kt

--- app/src/main/java/com/flower_tech/splash/SplashActivity.kt
+++ app/src/main/java/com/flower_tech/splash/SplashActivity.kt
@@ -3,15 +3,14 @@ package com.flower_tech.splash
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.flower_tech.auth.AuthActivity
+import com.flower_tech.main.MainActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val intent = Intent(this, AuthActivity::class.java)
+        val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
         finish()
-
     }
 
 }
