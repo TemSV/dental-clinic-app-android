7801b322f6c5e03f47e7eb590e731a21e1cf89d5
llav3ji2019 <pavelemelyanov03@mail.ru>
2023-03-26 15:24:35 +0300

    fix education

--- app/src/main/java/com/flower_tech/structures/Education.kt
+++ app/src/main/java/com/flower_tech/structures/Education.kt
@@ -2,10 +2,13 @@ package com.flower_tech.structures
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
+import java.io.File
 
 @Parcelize
-data class Education(
-    val degree: String,
-    val educationalInstitution: String,
-    val date: String
+data class Education (
+    var degree: String = "",
+    var educationalInstitution: String = "",
+    var specialization: String = "",
+    var file: String = "",
+    var date: String = ""
 ) : Parcelable
