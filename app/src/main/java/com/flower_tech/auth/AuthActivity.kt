e2dca1b11f9c2fafb446242ce5297c0374cede36
makniker <102602253+makniker@users.noreply.github.com>
2023-03-26 22:55:51 +0300

    SplashActivity.kt

--- app/src/main/java/com/flower_tech/auth/AuthActivity.kt
+++ app/src/main/java/com/flower_tech/auth/AuthActivity.kt
@@ -3,10 +3,13 @@ package com.flower_tech.auth
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.flower_tech.R
+import com.flower_tech.databinding.ActivityAuthBinding
 
 class AuthActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAuthBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
     }
 }
---
+++ app/src/main/java/com/flower_tech/auth/HelloFragment.kt
@@ -0,0 +1,17 @@
+package com.flower_tech.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.flower_tech.R
+
+class HelloFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_hello, container, false)
+    }
+}
