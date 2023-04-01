0c547e3b12186d872c746eaf4c18dd1427cd59dc
makniker <102602253+makniker@users.noreply.github.com>
2023-04-02 00:01:57 +0300

    часть регистрации

--- app/src/main/java/com/flower_tech/auth/SignInFragment.kt
+++ app/src/main/java/com/flower_tech/auth/SignInFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import com.flower_tech.R
 
 class SignInFragment : Fragment() {
@@ -13,7 +15,11 @@ class SignInFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_in, container, false)
+        val view = inflater.inflate(R.layout.fragment_log_in, container, false)
+        val entryButton = view.findViewById<Button>(R.id.EntryButtonToMainMenu)
+        entryButton.setOnClickListener{
+            findNavController().navigate(R.id.action_signIn2Fragment_to_mainActivity)
+        }
+        return view
     }
 }
