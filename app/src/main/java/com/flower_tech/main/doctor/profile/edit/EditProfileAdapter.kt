9dc1b7102ec8f01efd7ab16db6d2ddbcd9db81e2
llav3ji2019 <pavelemelyanov03@mail.ru>
2023-04-02 07:29:43 +0300

    fix profile edit

--- app/src/main/java/com/flower_tech/main/doctor/profile/edit/EditProfileAdapter.kt
+++ app/src/main/java/com/flower_tech/main/doctor/profile/edit/EditProfileAdapter.kt
@@ -1,31 +1,33 @@
 package com.flower_tech.main.doctor.profile.edit
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.EditText
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.flower_tech.R
 import com.flower_tech.structures.Education
 
-class EditProfileAdapter(private val educations: MutableList<Education>) :
+class EditProfileAdapter(private val educations: MutableList<Education>, private val context: Context) :
     RecyclerView.Adapter<EditProfileAdapter.EducationInfoItemViewHolder>() {
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ) = EducationInfoItemViewHolder(
-        LayoutInflater.from(parent.context).inflate(R.layout.item_edit_education, null)
+        LayoutInflater.from(parent.context).inflate(R.layout.item_edit_education, null),
+        context
     )
 
     override fun onBindViewHolder(
         holder: EducationInfoItemViewHolder,
         position: Int
     ) {
-        for ((id, name) in educations.withIndex()) {
-            Log.d("hehe", id.toString())
-        }
         val currentItem = if (educations.size > position) {
             educations[position]
         } else {
@@ -36,18 +38,45 @@ class EditProfileAdapter(private val educations: MutableList<Education>) :
 
     override fun getItemCount() = educations.size + 1
 
-    class EducationInfoItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    class EducationInfoItemViewHolder(view: View, private val context: Context) : RecyclerView.ViewHolder(view) {
         private val counter = view.findViewById<TextView>(R.id.education_list_counter)
         private val documentName = view.findViewById<TextView>(R.id.education_document_name)
-        private val specialization = view.findViewById<TextView>(R.id.specialization_input)
-        private val educationalInstitution = view.findViewById<TextView>(R.id.educational_institution_input)
+        private val specialization = view.findViewById<AutoCompleteTextView>(R.id.specialization_input)
+        private val educationalInstitution = view.findViewById<AutoCompleteTextView>(R.id.educational_institution_input)
+        private val yearsOfEducation = view.findViewById<EditText>(R.id.years_of_education_input)
 
         @SuppressLint("SetTextI18n")
         fun bind(item: Education, position: Int) {
+            yearsOfEducation.setText(item.date)
             counter.text = (position + 1).toString()
             documentName.text = item.file
-            specialization.text = item.specialization
-            educationalInstitution.text = item.educationalInstitution
+            educationalInstitution.setText(item.educationalInstitution)
+            specialization.setText(item.specialization)
+            specialization.setAdapter(ArrayAdapter(context, R.layout.item_autocomplete_list, R.id.text_view_list_item, getSpecializations()))
+            educationalInstitution.setAdapter(ArrayAdapter(context, R.layout.item_autocomplete_list, R.id.text_view_list_item, getEducationalInstitutions()))
+        }
+
+        private fun getSpecializations(): MutableList<String> {
+            return arrayListOf(
+                "Стоматолог-терапевт",
+                "Стоматолог-хирург",
+                "Стоматолог-ортопед",
+                "Стоматолог-парадонтолог",
+                "Ортодонт",
+            )
+        }
+
+        private fun getEducationalInstitutions():List<String> {
+            return arrayListOf(
+                "Северо-Западный институт управления РАНХиГС",
+                "Санкт-Петербургский государственный электротехнический университет «ЛЭТИ» им. В.И. Ульянова (Ленина)",
+                "Санкт-Петербургский Гуманитарный университет профсоюзов",
+                "Санкт-Петербургский государственный экономический университет",
+                "Национальный исследовательский университет «Высшая школа экономики» — Санкт-Петербург",
+                "Балтийский государственный технический университет «ВОЕНМЕХ» имени Д. Ф. Устинова",
+                "Санкт-Петербургский Горный университет",
+                "Государственный университет морского и речного флота имени адмирала С. О. Макарова",
+            )
         }
     }
 
--- app/src/main/java/com/flower_tech/main/doctor/profile/edit/EditProfileFragment.kt
+++ app/src/main/java/com/flower_tech/main/doctor/profile/edit/EditProfileFragment.kt
@@ -2,9 +2,11 @@ package com.flower_tech.main.doctor.profile.edit
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -16,7 +18,7 @@ import com.flower_tech.structures.Education
 
 class EditProfileFragment : Fragment() {
     private lateinit var binding: FragmentEditProfileBinding
-    private val data: MutableList<Education> = ArrayList<Education>()
+    private val data: MutableList<Education> = getEducation()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,6 +35,11 @@ class EditProfileFragment : Fragment() {
             setUpAddButton(addDocumentBtn)
             profileEditAppBar.setNavigationOnClickListener {
                 findNavController().navigate(R.id.action_fragment_edit_profile_container_to_fragment_profile_information_container)
+            }
+            context?.let {
+                cityInput.setAdapter(ArrayAdapter(it, R.layout.item_autocomplete_list, R.id.text_view_list_item, getCountries()))
+                districtList.setAdapter(ArrayAdapter(it, R.layout.item_autocomplete_list, R.id.text_view_list_item, getSaintPetersburgCountries()))
+                clinicAddressInput.setAdapter(ArrayAdapter(it, R.layout.item_autocomplete_list, R.id.text_view_list_item, getAddresses()))
             }
         }
 
@@ -68,7 +75,7 @@ class EditProfileFragment : Fragment() {
         recyclerView.apply {
             setHasFixedSize(true)
             isNestedScrollingEnabled = false
-            adapter = EditProfileAdapter(data)
+            adapter = EditProfileAdapter(data, context)
             layoutManager = LinearLayoutManager(context)
         }
     }
@@ -76,6 +83,52 @@ class EditProfileFragment : Fragment() {
     companion object {
         @JvmStatic
         fun newInstance() = EditProfileFragment()
+    }
+
+    private fun getAddresses() : MutableList<String> {
+        return arrayListOf(
+            "Политехническая 29",
+            "Большой проспект Васильевского острова, 15",
+            "улица Лизы Чайкиной, 19",
+            "улица Ленина, 48"
+        )
+    }
+
+    private fun getCountries(): MutableList<String>{
+        return arrayListOf(
+            "Санкт-Петербург",
+            "Абакан",
+            "Брянск",
+            "Вологда",
+            "Грозный",
+            "Екатеринбург",
+            "Ейск",
+            "Кемерово",
+            "Курск",
+            "Комсомольск-на-Амуре",
+        )
+    }
+    private fun getSaintPetersburgCountries(): MutableList<String>{
+        return arrayListOf(
+            "Адмиралтейский район",
+            "Василеостровский район",
+            "Выборгский район",
+            "Калининский район",
+            "Кировский район",
+            "Колпинский район",
+            "Колпинский район",
+            "Красносельский район",
+            "Кронштадтский район",
+            "Курортный район",
+            "Московский район",
+            "Московский район",
+            "Петроградский район",
+            "Петродворцовый район",
+            "Приморский район",
+            "Пушкинский район",
+            "Фрунзенский район",
+            "Центральный район"
+        )
     }
 
     private fun getEducation(): MutableList<Education> {
