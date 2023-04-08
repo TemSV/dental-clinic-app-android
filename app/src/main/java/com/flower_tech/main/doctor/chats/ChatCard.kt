12e8d2b1983f3d002fb30dcf652e15eddde8731b
TemSV <artemsviridov09@gmail.com>
2023-04-08 15:11:19 +0300

    added new data class

---
+++ app/src/main/java/com/flower_tech/main/doctor/chats/ChatCard.kt
@@ -0,0 +1,8 @@
+package com.flower_tech.main.doctor.chats
+
+data class ChatCard (
+    val chatPatientIcon: Int,
+    val chatPatientName: String,
+    val chatText: String,
+    val chatDate: String,
+)
