907207095886fce82367e8a89ead9baef257cb25
makniker <102602253+makniker@users.noreply.github.com>
2023-04-09 12:45:01 +0300

    лого, сплэш

--- app/src/main/java/com/flower_tech/splash/SplashActivity.kt
+++ app/src/main/java/com/flower_tech/splash/SplashActivity.kt
@@ -4,13 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import com.flower_tech.auth.AuthActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val intent = Intent(this, AuthActivity::class.java)
-        startActivity(intent)
-        finish()
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                val intent = Intent(this, AuthActivity::class.java)
+                startActivity(intent)
+            },
+            3000)
     }
 }
