18327ba28bed07829fa920d6f75524396f33ebdc
makniker <102602253+makniker@users.noreply.github.com>
2023-04-22 21:44:58 +0300

    осталось ток пофиксить неправильную тень для глаза и найти правильный стейт, чтобы после нажатия он оставался зачеркнутым(или поставить дефолтный глаз, но он будет не по центру поля по высоте, а только рядом с текстом). Есть бага с тем, что подсказка для выбора города при свертывании клавиатуры улетает вверх(т.к поле слишком низко, клавиатура перекрывает выпадающий список, установил в координатах смещение вверх, поищ), завтра буду досматривать, как это все пофиксить или сделать по-другому

--- app/src/main/java/com/flower_tech/auth/UserDataFragment.kt
+++ app/src/main/java/com/flower_tech/auth/UserDataFragment.kt
@@ -1,14 +1,16 @@
 package com.flower_tech.auth
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.flower_tech.R
 import com.flower_tech.databinding.FragmentUserDataBinding
 import com.google.android.material.color.MaterialColors
+
 
 class UserDataFragment : Fragment() {
     private lateinit var binding: FragmentUserDataBinding
@@ -22,6 +24,16 @@ class UserDataFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        with(binding) {
+            btnSwitch.setOnClickListener {
+                findNavController().navigate(R.id.action_fragment_user_data_container_to_fragment_account_registration_container)
+            }
+        }
+        setUpAutoCompleteText()
+        setUpGenderButton(view)
+    }
+
+    private fun setUpGenderButton(view: View) {
         with(binding) {
             btnFemale.isEnabled = true
             btnMale.isEnabled = false
@@ -39,10 +51,36 @@ class UserDataFragment : Fragment() {
                 btnFemale.setTextColor(MaterialColors.getColor(view, R.attr.text_color_primary))
                 btnMale.setTextColor(MaterialColors.getColor(view, R.attr.text_color_tertiary))
             }
-            btnSwitch.setOnClickListener {
-                findNavController().navigate(R.id.action_fragment_user_data_container_to_fragment_account_registration_container)
+        }
+    }
+
+    private fun setUpAutoCompleteText() {
+        with(binding) {
+            context?.let {
+                editCityRegData.setAdapter(
+                    ArrayAdapter(
+                        it,
+                        R.layout.item_autocomplete_list,
+                        R.id.text_view_list_item,
+                        getCountries()
+                    )
+                )
             }
         }
+    }
 
+    private fun getCountries(): MutableList<String> {
+        return arrayListOf(
+            "Санкт-Петербург",
+            "Абакан",
+            "Брянск",
+            "Вологда",
+            "Грозный",
+            "Екатеринбург",
+            "Ейск",
+            "Кемерово",
+            "Курск",
+            "Комсомольск-на-Амуре",
+        )
     }
 }
